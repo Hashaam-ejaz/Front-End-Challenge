f2acf35362f152f4d9d756f9185de4e3954b7534
HashaamBinEjaz <139843321+HashaamBinEjaz@users.noreply.github.com>
2024-06-28 01:19:23 +0500

    Some styling for filter modal

--- app/components/CategoryFilterModal.tsx
+++ app/components/CategoryFilterModal.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 interface CategoryFilterModalProps {
   showModal: boolean;
-  onSelectCategory: (category: string) => void;
+  onSelectCategory: (category: string | null) => void; // Updated to accept null
   onCloseModal: () => void;
   categories: string[];
 }
@@ -15,7 +15,11 @@ const CategoryFilterModal: React.FC<CategoryFilterModalProps> = ({
 }) => {
   const handleCategorySelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedCategory = e.target.value;
-    onSelectCategory(selectedCategory);
+    if (selectedCategory === "") {
+      onSelectCategory(null); // Handle "None" option
+    } else {
+      onSelectCategory(selectedCategory);
+    }
   };
 
   // Add event listener when component mounts to handle click outside modal
@@ -44,19 +48,33 @@ const CategoryFilterModal: React.FC<CategoryFilterModalProps> = ({
         showModal ? "" : "hidden"
       }`}
     >
-      <div className="bg-white p-4 rounded-lg">
-        <div className="text-lg font-semibold mb-2">Filter by Category</div>
-        <select
-          className="w-full p-2 border border-gray-300 rounded-md"
-          onChange={handleCategorySelect}
-        >
-          <option value="">Select Category</option>
-          {categories.map((category, index) => (
-            <option key={index} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
+      <div className="bg-white p-4 rounded-lg w-[15.625rem] h-[18rem] flex flex-col ml-[1.25rem]">
+        <div className="text-sm font-normal  mt-[1.063rem] mb-[0.75rem]">
+          Category
+        </div>
+        <div className="flex mb-[1.625rem]">
+          <select
+            className="w-full p-2 border border-gray-300 rounded-md"
+            onChange={handleCategorySelect}
+          >
+            <option defaultChecked value="">
+              None
+            </option>{" "}
+            {/* Add "None" option */}
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex font-normal text-sm mb-[0.813rem]">
+          Date & Time
+        </div>
+        <div className="flex mb-[0.375rem]">From</div>
+        <div className="flex">Date from Dropdown</div>
+        <div className="flex mb-[0.375rem] mt-[0.75rem]">To</div>
+        <div className="flex">Date To Dropdown</div>
       </div>
     </div>
   );
